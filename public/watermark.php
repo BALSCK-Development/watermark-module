40dc571544585a2eadb298867f581df7bb6f2624
lsevcik <lubomir.sevcik@salesxp.com>
2021-12-11 18:04:03 +0100

    idk

--- public/watermark.php
+++ public/watermark.php
@@ -4,14 +4,14 @@
 
 function addWatermark($nameFile,$timestamp, $imageDir, $watermarkDir,$imageFileType)
 {
-
-
     $image = '';
 
     if($imageFileType == 'png')
     {
         $image = imagecreatefrompng($imageDir);
         $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
+        // TODO we need transparent background here
+        // TODO dont create white background. Background can be image where we want paste watermark
         imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
         imagealphablending($bg, TRUE);
         imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
@@ -24,13 +24,7 @@ function addWatermark($nameFile,$timestamp, $imageDir, $watermarkDir,$imageFileT
         $image = imagecreatefromjpeg($imageDir);
     }
 
-    //$image = imagecreatefrompng($imageDir);
     $watermark = imagecreatefrompng($watermarkDir);
-
-
-    //TODO work only for jpg
-    //$image = imagecreatefromjpeg($imageDir);                                //load image where u want add watermark
-    //$watermark = imagecreatefromjpeg($watermarkDir);                        // load watermark
 
     $image_width = imagesx($image);                                         // get image width
     $image_height = imagesy($image);                                        //get image height
@@ -52,13 +46,15 @@ function addWatermark($nameFile,$timestamp, $imageDir, $watermarkDir,$imageFileT
         $resized_watermark_width = imagesx($resized_watermark);
         $resized_watermark_height = imagesy($resized_watermark);
         imagecopymerge($image,$resized_watermark,0,0,-($image_width-$image_height)/2,0,$resized_watermark_width,$resized_watermark_height,30);    //copy watermark to image
+
     }else{
         imagecopyresampled($resized_watermark, $watermark, 0, 0, 0, 0, $image_width, $image_height, $watermark_width, $watermark_height);
         $resized_watermark_width = imagesx($resized_watermark);
         $resized_watermark_height = imagesy($resized_watermark);
         imagecopymerge($image,$resized_watermark,0,0,0,0,$resized_watermark_width,$resized_watermark_height,30);    //copy watermark to image
+
     }
+
     imagejpeg($image,'watermarked/'.$timestamp."_".$nameFile.'.jpg');    //create new image with watermark
-    //imagepng($image,'watermarked/'.$timestamp."_".$nameFile);
 }
 
